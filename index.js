ab18275c0e2452571b35137bb50e7e4f1420ff13
DinisValentim <dinisvalentim1504@gmail.com>
2026-01-15 18:07:40 +0100

    Co-authored-by: MiguelRP080 <MiguelRP080@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('Hello, World!');
+
+class Shop{
+    constructor(name, address, id) {
+        this.name = name
+        this.address = address
+        this.id = id
+    }
+}
+
+class Products {
+    constructor(id, name, price, version, category) {
+        this.id = id
+        this.name = name
+        this.price = price
+        this.version = version
+        this.category = category
+    }
+}
+
+class Inventory {
+    constructor(products, quantity) {
+        this.products = products
+        this.quantity = quantity
+    }
+}
+
+class ShopCart {
+    constructor(product, productID, quantity, price, totalPrice){
+        this.product = product
+        this.productID = productID
+        this.quantity = quantity
+        this.price = price
+        this.totalPrice = totalPrice
+    }
+}
+
+class Order {
+    constructor(product, productID, ) {
+        
+    }
+}
