eb0a9d764f245ce3c366750e9bb7fb1d7b31b2c9
DinisValentim <dinisvalentim1504@gmail.com>
2026-01-16 11:48:08 +0100

    Created ShowProduct and addProduct

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ class Shop{
 }
 
 class Products {
+
     constructor(id, name, price, version, category) {
         this.id = id
         this.name = name
@@ -18,9 +19,23 @@ class Products {
 }
 
 class Inventory {
-    constructor(products, quantity) {
-        this.products = products
-        this.quantity = quantity
+
+    constructor(quantity) {
+        this.products = []
+    }
+
+    ShowProduct(productID){
+        let prdct = this.products.find((Prdct)=>Prdct.id === productID)
+        console.log(prdct.name)
+        console.log(prdct.id)
+        console.log(prdct.price + "€")
+        console.log("Version: "+prdct.version)
+        console.log("Category: "+prdct.category)
+        return prdct
+    }
+
+    addProduct(product){
+        this.products.push(product)
     }
 }
 
@@ -38,7 +53,7 @@ class Order {
         this.product = product
         this.productID = productID
         this.customerID = customerID
-        this. quantity = quantity
+        this.quantity = quantity
         this.price = price
         this.totalPrice = totalPrice
     }
@@ -51,6 +66,14 @@ class Customer {
     }
 }
 
+Pc = new Products('1','PC',300,'2.1','PCs')
+const duck = new Products('2','Duck',3001,'2.1','Ducks')
+const inventory = new Inventory()
+inventory.addProduct(duck)
+
+
+
+setTimeout(() => inventory.ShowProduct(duck.id), 1000)
 
 
 
