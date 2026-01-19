cbc048a4e27868107c220f8b8f482ad209e56bb1
MiguelRP080 <miguelrpestana.08@gmail.com>
2026-01-19 14:41:14 +0100

    calculateTotal, createInvoice and buyProducts Methods Bug Fixes and tiny changes

---
+++ data.js
@@ -0,0 +1,58 @@
+export class Products {
+    constructor(id, name, price, brand, version, category) {
+        this.id = id
+        this.name = name
+        this.price = price
+        this.brand = brand
+        this.version = version
+        this.category = category
+    }
+}
+
+//====================================================================================================================================|
+// Products                                                                                                                           |
+//====================================================================================================================================|
+
+
+// Smartphones =======================================================================================================================|
+
+// Samsung
+const samsungS24 = new Products (1,'Smartphone Samsung Galaxy S24 (6.2\'\' - 8GB - 256GB)', 699.99, 'Samsung', 'S24', 'Smartphone')
+const samsungS25 = new Products (2,'Smartphone Samsung Galaxy S25 (6.2\'\' - 12GB - 256GB)', 999.99, 'Samsung', 'S25', 'Smartphone')
+const samsungA25 = new Products (3,'Smartphone Samsung Galaxy A25 (6.5\'\' - 8GB - 256GB)', 249.99, 'Samsung', 'A25', 'Smartphone')
+const samsungA35 = new Products (4,'Smartphone Samsung Galaxy A35 (6.6\'\' - 6GB - 128GB)', 329.99, 'Samsung', 'A35', 'Smartphone')
+const samsungZFlip6 = new Products (5,'Smartphone Samsung Galaxy Z Flip 6 (6.7\'\' - 12GB - 256GB)', 1199.00, 'Samsung', 'Z Flip 6', 'Smartphone')
+const samsungZFold6 = new Products (6,'Smartphone Samsung Galaxy Z Fold 6 (7.6\'\' - 12GB - 256GB)', 1999.00, 'Samsung', 'Z Fold 6', 'Smartphone')
+
+// Iphone
+const iphone14 = new Products (7,'iPhone 14 (6.1\'\' - 128GB)', 699.00, 'APPLE', '14', 'Smartphone')
+const iphone14ProMax = new Products (8,'iPhone 14 Pro Max (6.7\'\' - 128GB)', 1099.00, 'APPLE', '14 Pro Max', 'Smartphone')
+const iphone15 = new Products (9,'iPhone 15 (6.1\'\' - 128GB)', 799.00, 'APPLE', '15', 'Smartphone')
+const iphone15ProMax = new Products (10,'iPhone 15 Pro Max (6.7\'\' - 256GB)', 1299.00, 'APPLE', '15 Pro Max', 'Smartphone')
+const iphone16 = new Products (11,'iPhone 16 (6.1\'\' - 128GB)', 929.00, 'APPLE', '16', 'Smartphone')
+const iphone16ProMax = new Products (12,'iPhone 16 Pro Max (6.7\'\' - 128GB)', 1499.00, 'APPLE', '16 Pro Max', 'Smartphone')
+
+// Xiaomi
+const xiaomi13 = new Products(13,'Smartphone XIAOMI 13 (6.36\'\' - 8GB - 256GB)',649.99,'XIAOMI','13','Smartphone')
+const xiaomi14 = new Products(14,'Smartphone XIAOMI 14 (6.36\'\' - 12GB - 256GB)',899.99,'XIAOMI','14','Smartphone')
+const xiaomiRedmi12 = new Products (15,'Smartphone XIAOMI Redmi 12 (6.67\'\' - 8GB - 256GB)',169.99, 'XIAOMI', 'Redmi 12', 'Smartphone')
+const xiaomiRedmi13 = new Products (16,'Smartphone XIAOMI Redmi 13 (6.79\'\' - 8GB - 256GB)',199.99, 'XIAOMI', 'Redmi 13', 'Smartphone')
+const xiaomiRedmiNote13 = new Products (17,'Smartphone XIAOMI Redmi Note 13 (6.67\'\' - 12GB - 512GB)',299.99, 'XIAOMI', 'Redmi Note 13', 'Smartphone')
+const xiaomiRedmiNote14 = new Products (18,'Smartphone XIAOMI Redmi Note 14 (6.67\'\' - 8GB - 256GB)',249.99, 'XIAOMI', 'Redmi Note 14', 'Smartphone')
+
+
+//====================================================================================================================================|
+
+
+
+//====================================================================================================================================|
+//                                                                                                                                    |
+//====================================================================================================================================|
+
+
+
+export const productsList = [
+    [samsungS24, 2], [samsungS25, 3], [samsungA25, 3], [samsungA35, 2], [samsungZFlip6, 1], [samsungZFold6, 2],
+    [iphone14, 5], [iphone14ProMax, 4], [iphone15, 2], [iphone15ProMax, 5], [iphone16, 2], [iphone16ProMax, 5],
+    [xiaomi13, 6], [xiaomi14, 2], [xiaomiRedmi12, 5], [xiaomiRedmi13, 2], [xiaomiRedmiNote13, 3], [xiaomiRedmiNote14, 4]
+]
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { productsList } from "./data.js"
 
 class Shop{
     constructor(name, address, id) {
@@ -7,30 +8,129 @@ class Shop{
     }
 }
 
-class Products {
-    constructor(id, name, price, brand, version, category) {
-        this.id = id
-        this.name = name
-        this.price = price
-        this.brand = brand
-        this.version = version
-        this.category = category
-    }
-}
-
 class Inventory {
-    constructor(products, quantity) {
-        this.products = products
-        this.quantity = quantity
+
+    constructor(quantity) {
+        this.products = new Map()
+    }
+
+    ShowProduct(productID){
+
+        if (!clear) {
+            console.clear("")
+            clear = true
+        }
+        for (let product of this.products.keys()) {
+            if (product.id === productID) {
+                let prdct = product
+                console.log("\n|--------------------------------------------------------------------------------|")
+                console.log("| \t\t\t\t\t", prdct.id, "\t\t\t\t\t |")
+                console.log("|--------------------------------------------------------------------------------|")
+                console.log(" ", prdct.name)
+                console.log(" ", prdct.price + " €")
+                console.log("  Brand: " + prdct.brand)
+                console.log("  Version: " + prdct.version)
+                console.log("  Category: " + prdct.category)
+                console.log("  Quantity: " + this.products.get(product))
+                console.log("|--------------------------------------------------------------------------------|\n")
+                return prdct
+            }
+        }
+        return null
+    }
+
+    addProduct(product, quantity) {
+        if (this.products.has(product)) {
+            this.products.set(product, this.products.get(product) + quantity)
+        } else {
+            this.products.set(product, quantity)
+        }
+    }
+
+    removeProduct(product){
+        console.log("You deleted the product: " + product.name)
+        this.products.delete(product)
+    }
+
+    showListOfProducts(){
+        this.products.forEach((quantity, product) => {
+            console.log("Product Name: " + product.name + " | ID: " + product.id + " | Quantity: " + quantity)
+        });
+    }
+
+    ChangeProducts(product, type, newValue){
+        switch (type) {
+            case "Name":
+                product.name = newValue
+                break;
+            case "Price":
+                product.price = parseFloat(newValue)
+                break;
+            case "Brand":
+                product.brand = newValue
+                break;
+            case "Version":
+                product.version = newValue
+                break;
+            case "Category":
+                product.category = newValue
+                break;
+            case "Quantity":
+                this.products.set(product, parseInt(newValue))
+                break;
+            default:
+                console.log("Invalid type specified.")
+                return;
+        }
+    }
+
+    UpdateProduct(product, name, price, brand, version, category, quantity){
+        product.name = name
+        product.price = price
+        product.brand = brand
+        product.version = version
+        product.category = category
+        this.products.set(product, quantity)
+
+    }
+
+    getProduct(id){
+        for (let product of this.products.keys()) {
+            if (product.id === id) {
+                return product
+            }
+        }
     }
 }
 
 class ShoppingCart {
-    constructor(product, productID, quantity, price){
-        this.product = product
-        this.productID = productID
+    
+    constructor(product, id, quantity, price) {
+        this.products = new Map()
+        this.products.set(product, quantity)
+        this.id = id
         this.quantity = quantity
         this.price = price
+    }
+
+
+    showCart(productID){
+        this.products.forEach((quantity, product) => {
+            if (product.id === productID) {
+                let prdct = product
+                console.log("\n|--------------------------------------------------------------------------------|")
+                console.log("| \t\t\t\t\t", prdct.id, "\t\t\t\t\t |")
+                console.log("|--------------------------------------------------------------------------------|")
+                console.log(" ", prdct.name)
+                console.log(" ", prdct.price + " €")
+                console.log("  Brand: " + prdct.brand)
+                console.log("  Version: " + prdct.version)
+                console.log("  Category: " + prdct.category)
+                console.log("  Quantity: " + quantity)
+                console.log("|--------------------------------------------------------------------------------|\n")
+                return prdct
+            }
+        });
     }
 }
 
@@ -39,49 +139,92 @@ class Order {
         this.product = product
         this.productID = productID
         this.customerID = customerID
-        this. quantity = quantity
+        this.quantity = quantity
         this.price = price
         this.totalPrice = totalPrice
+    }
+
+    calculateTotal(cart) {
+        fullPrice = 0
+        for (let item of cart) {
+            for (let product of item.products.keys()) {
+                fullPrice +=  item.price * item.quantity
+                this.totalPrice = fullPrice
+            }
+        }
+    }
+
+    createInvoice(cart, name, id) {
+        this.customerID = id
+        console.log("\n|-------------------------------------------------------------------------------|")
+        console.log("|\t\t\t\t " + name + "'s Invoice \t\t\t\t|")
+        console.log("|-------------------------------------------------------------------------------|")
+        for (let item of cart) {
+            for (let product of item.products.keys()) {
+                fullPrice = item.price * item.quantity
+                console.log(" ", product.name)
+                console.log("  ID: " + item.id)
+                console.log("  Quantity: " + item.quantity)
+                console.log("  Price (Each One): ", product.price + " €   | Price (All): ", fullPrice + " €\n")   
+            }
+        }
+        console.log("  Total: ", this.totalPrice + " €\n")
+        console.log("\n  NIF: " + this.customerID)
+        console.log("|-------------------------------------------------------------------------------|")
+        this.totalPrice = 0
     }
 }
 
 class Customer {
-    constructor(name, id) {
+    constructor(name, id, cart) {
         this.name = name
-        this.id = id    
+        this.id = id
+        this.cart = [];
+    }
+          
+    addToCart(product, quantity){
+        this.cart.push(new ShoppingCart(product, product.id, quantity, product.price))
+        return product
+    }
+
+    removeFromCart(productID){
+        for (let item of this.cart) {
+            for (let product of item.products.keys()) {
+                if (product.id === productID) {
+                    item.products.delete(product)
+                    return
+                }
+            }
+        }
+    }
+
+    buyProducts(){
+        let emptyCart = []
+        let order = new Order(this.cart)
+        order.calculateTotal(this.cart)
+        order.createInvoice(this.cart, this.name, this.id)
+        this.cart = emptyCart;
     }
 }
 
+let prdct
+let qntt
+let clear = false
+let fullPrice = 0
 
+const inventory = new Inventory()
 
-//====================================================================================================================================|
-// Products                                                                                                                           |
-//====================================================================================================================================|
+productsList.forEach(product => {
+    prdct = product[0]
+    qntt = product[1] 
+    inventory.addProduct(prdct, qntt)
+});
 
-//------------------------------------------------------------------------------------------------------------------------------------|
-// Smartphones                                                                                                                        |
-//------------------------------------------------------------------------------------------------------------------------------------|
+const Frederico = new Customer("Frederico", 795304197, null)
+Frederico.addToCart(inventory.getProduct(2), 1)
+Frederico.addToCart(inventory.getProduct(12),2)
+Frederico.buyProducts()
 
-// Samsung
-const samsungS24 = new Products (1,'Smartphone Samsung Galaxy S24 (6.2\'\' - 8GB - 256GB)', 699.99, 'Samsung', 'S24', 'Smartphone')
-const samsungS25 = new Products (2,'Smartphone Samsung Galaxy S25 (6.2\'\' - 12GB - 256GB)', 999.99, 'Samsung', 'S25', 'Smartphone')
-const samsungA25 = new Products (3,'Smartphone Samsung Galaxy A25 (6.5\'\' - 8GB - 256GB)', 249.99, 'Samsung', 'A25', 'Smartphone')
-const samsungA35 = new Products (4,'Smartphone Samsung Galaxy A35 (6.6\'\' - 6GB - 128GB)', 329.99, 'Samsung', 'A35', 'Smartphone')
-const samsungZFlip6 = new Products (5,'Smartphone Samsung Galaxy Z Flip 6 (6.7\'\' - 12GB - 256GB)', 1199.00, 'Samsung', 'Z Flip 6', 'Smartphone')
-const samsungZFold6 = new Products (6,'Smartphone Samsung Galaxy Z Fold 6 (7.6\'\' - 12GB - 256GB)', 1999.00, 'Samsung', 'Z Fold 6', 'Smartphone')
-
-// Iphone
-const iphone14 = new Products (7,'iPhone 14 (6.1\'\' - 128GB)', 699.00, 'APPLE', '14', 'Smartphone')
-const iphone14ProMax = new Products (8,'iPhone 14 Pro Max (6.7\'\' - 128GB)', 1099.00, 'APPLE', '14 Pro Max', 'Smartphone')
-const iphone15 = new Products (9,'iPhone 15 (6.1\'\' - 128GB)', 799.00, 'APPLE', '15', 'Smartphone')
-const iphone15ProMax = new Products (10,'iPhone 15 Pro Max (6.7\'\' - 256GB)', 1299.00, 'APPLE', '15 Pro Max', 'Smartphone')
-const iphone16 = new Products (11,'iPhone 16 (6.1\'\' - 128GB)', 929.00, 'APPLE', '16', 'Smartphone')
-const iphone16ProMax = new Products (12,'iPhone 16 Pro Max (6.7\'\' - 128GB)', 1499.00, 'APPLE', '16 Pro Max', 'Smartphone')
-
-// Xiaomi
-const xiaomi13 = new Products(13,'Smartphone XIAOMI 13 (6.36\'\' - 8GB - 256GB)',649.99,'XIAOMI','13','Smartphone')
-const xiaomi14 = new Products(14,'Smartphone XIAOMI 14 (6.36\'\' - 12GB - 256GB)',899.99,'XIAOMI','14','Smartphone')
-const xiaomiRedmi12 = new Products (15,'Smartphone XIAOMI Redmi 12 (6.67\'\' - 8GB - 256GB)',169.99, 'XIAOMI', 'Redmi 12', 'Smartphone')
-const xiaomiRedmi13 = new Products (16,'Smartphone XIAOMI Redmi 13 (6.79\'\' - 8GB - 256GB)',199.99, 'XIAOMI', 'Redmi 13', 'Smartphone')
-const xiaomiRedmiNote13 = new Products (17,'Smartphone XIAOMI Redmi Note 13 (6.67\'\' - 12GB - 512GB)',299.99, 'XIAOMI', 'Redmi Note 13', 'Smartphone')
-const xiaomiRedmiNote14 = new Products (18,'Smartphone XIAOMI Redmi Note 14 (6.67\'\' - 8GB - 256GB)',249.99, 'XIAOMI', 'Redmi Note 14', 'Smartphone')
+const Alberto = new Customer("Alberto", 492164829, null)
+Alberto.addToCart(inventory.getProduct(8), 1)
+Alberto.buyProducts()
